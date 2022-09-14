ccdeeecf4f749dfb5dfc70a452ce369f8887d9ad
Petr Sulc <psulc@asu.edu>
2022-09-14 17:35:10 +0200

    Added custom coaxial stacking option; added new contrib/sulc/ interactions; Currently does not compile and full of bugs

---
+++ contrib/sulc/src/Particles/TorsionalPatchyParticle.h
@@ -0,0 +1,62 @@
+/*
+ * TorsionalPatchyParticle.h
+ *
+ *  Created on: 15/mar/2013
+ *      Author: lorenzo
+ */
+
+#ifndef TTorsionalPatchyParticle_H_
+#define TTorsionalPatchyParticle_H_
+
+#include "../../../../src/Particles/BaseParticle.h"
+
+struct PatchyBond {
+	BaseParticle *other;
+	number r_p;
+	int p_patch, q_patch;
+	number energy;
+	LR_vector force;
+	LR_vector p_torque, q_torque;
+
+	PatchyBond(BaseParticle *o, number my_r_p, int pp, int qp, number e) :
+					other(o),
+					r_p(my_r_p),
+					p_patch(pp),
+					q_patch(qp),
+					energy(e) {
+	}
+};
+
+/**
+ * @brief Incapsulates a patchy particle.
+ */
+
+class TorsionalPatchyParticle : public BaseParticle {
+public:
+	number _sigma;
+	std::vector<LR_vector> _base_patches;
+	std::vector<LR_vector> _a1_patches;  //a1 vector that is to be compared against the vector connecting the patches r_pp, needs to be parallel
+    std::vector<LR_vector> _a2_patches; // vector which needs to be parallel with a2 on the complementary patch
+	
+
+
+public:
+	TorsionalPatchyParticle(std::vector<LR_vector> base_patches, int nt, number sigma);
+	TorsionalPatchyParticle(int N_patches, int nt, number sigma);
+	virtual ~TorsionalPatchyParticle();
+
+	void set_positions();
+
+
+	virtual bool is_rigid_body() {
+		return true;
+	}
+
+	std::vector<LR_vector> base_patches() {
+		return _base_patches;
+	}
+
+	std::vector<PatchyBond> bonds;
+};
+
+#endif /* TorsionalPatchyParticle_H_ */
